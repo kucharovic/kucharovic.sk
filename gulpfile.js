b68626fb6cb6252ee9edc208508618b5311a93f4
Jakub Kucharovic <jkucharovic@gmail.com>
2015-04-20 15:21:03 +0200

    Added google prettify (via bower)

--- gulpfile.js
+++ gulpfile.js
@@ -11,4 +11,17 @@ gulp.task('assets', function() {
       gulp.dest('web/assets')
     )
   ;
+  gulp.src([
+    'bower_components/google-code-prettify/bin/prettify.min.css',
+    'bower_components/google-code-prettify/styles/sunburst.css'
+    ])
+    .pipe(
+      gulp.dest('web/assets/css')
+    )
+  ;
+  gulp.src('bower_components/google-code-prettify/bin/prettify.min.js')
+    .pipe(
+      gulp.dest('web/assets/js')
+    )
+  ;
 });
