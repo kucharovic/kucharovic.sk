572c697005d3dfe461f3d3f055057797de937b0c
Jakub Kucharovic <jkucharovic@gmail.com>
2015-04-27 09:23:38 +0200

    Added some styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,45 @@
-var gulp = require('gulp')
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    cssmin = require('gulp-cssmin'),
+    rename = require('gulp-rename')
 ;
 
-gulp.task('default', function() {
-    // No default task here
-});
-
-gulp.task('assets', function() {
-  gulp.src('app/Resources/public/**')
+gulp.task('styles', function() {
+  gulp.src('app/Resources/public/less/main.less')
+    .pipe(less())
+    .pipe(cssmin())
+    .pipe(rename({basename: 'compiled', suffix: '.min'}))
     .pipe(
-      gulp.dest('web/assets')
+      gulp.dest('web/assets/css')
     )
   ;
   gulp.src([
+    'bower_components/typeplate-starter-kit/css/typeplate.css',
     'bower_components/google-code-prettify/bin/prettify.min.css',
     'bower_components/google-code-prettify/styles/sunburst.css'
     ])
     .pipe(
       gulp.dest('web/assets/css')
+    )
+  ;
+  gulp.src('app/Resources/public/css/*')
+    .pipe(
+      gulp.dest('web/assets/css')
+    )
+  ;
+});
+gulp.task('images', function() {
+  gulp.src(
+    'app/Resources/public/img/*')
+    .pipe(
+      gulp.dest('web/assets/img')
+    )
+  ;
+});
+gulp.task('scripts', function() {
+  gulp.src('app/Resources/public/js/*')
+    .pipe(
+      gulp.dest('web/assets/js')
     )
   ;
   gulp.src('bower_components/google-code-prettify/bin/prettify.min.js')
@@ -25,3 +48,5 @@ gulp.task('assets', function() {
     )
   ;
 });
+
+gulp.task('default', ['styles', 'scripts', 'images']);
