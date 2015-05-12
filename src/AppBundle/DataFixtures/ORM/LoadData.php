fa3a7c30ab0eb3739f730dab1b8ef859e8452e0b
Jakub Kucharovic <jkucharovic@gmail.com>
2015-05-12 08:59:59 +0200

    Added information about post modification

--- src/AppBundle/DataFixtures/ORM/LoadData.php
+++ src/AppBundle/DataFixtures/ORM/LoadData.php
@@ -31,7 +31,8 @@ class LoadData implements FixtureInterface
         $post->setDescription('Lorem ipsum dolor sit amet.');
         $post->setPerex('<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquam, metus ultrices malesuada imperdiet, mauris leo volutpat ex, pellentesque tempus eros odio eu quam.');
         $post->setContent('<p>Ut malesuada ultrices tortor a mattis. Donec vel arcu at lorem dapibus bibendum non vitae ipsum. Praesent nec lectus vel magna molestie malesuada. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.<p>Proin hendrerit, nibh nec finibus venenatis, quam felis scelerisque velit, ut suscipit nunc elit eget purus. Sed leo ligula, porta hendrerit quam a, efficitur semper lectus. Donec rutrum finibus mauris ut porta. Sed in quam sit amet mauris porttitor rhoncus nec ac mi.');
-        $post->setPublishedAt(new \DateTime());
+        $post->setPublishedAt(new \DateTime(1431457200));
+        $post->setModifiedAt(new \DateTime(1431459000));
         $post->addTag($tag);
 
         $manager->persist($tag);
